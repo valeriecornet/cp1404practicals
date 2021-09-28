656df4b02560179ad06f008e922a2ef78d7721f4
valerie <valerie.cornet@my.jcu.edu.au>
2021-09-28 17:25:39 +1000

    Finish python file 3

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,14 +1,24 @@
 """Ask user for score and return the result"""
 
-score = float(input("Enter score: "))
-if 100 < score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
+
+def main():
+    score = float(input("Enter score: "))
+    result = calculate_status(score)
+    print(result)
+
+
+def calculate_status(score):
+    if 100 < score < 0:
         print("Invalid score")
-    elif score >= 50:
-        print("Passable")
-    elif score >= 90:
-        print("Excellent")
-    elif score < 50:
-        print("Bad")
+    else:
+        if 0 > score > 100:
+            print("Invalid score")
+        elif score >= 50:
+            print("Passable")
+        elif score >= 90:
+            print("Excellent")
+        elif score < 50:
+            print("Bad")
+
+
+main()
