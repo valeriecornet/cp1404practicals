be8af31272ebbc7e00b81b0ca314223dda450679
valerie <valerie.cornet@my.jcu.edu.au>
2021-09-30 09:32:07 +1000

    Add list warmup answers

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,18 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""Answers:
+numbers[0] = 3
+numbers[-1] = 2
+numbers[3] = 1
+numbers[:-1] = [3, 1, 4, 1, 5, 9 ,2] 
+numbers[3:4] = [1, 5]
+5 in numbers = numbers[4]
+7 in numbers = numbers[0] + numbers[2]
+"3" in numbers = char(numbers[0])
+numbers + [6, 5, 3] = numbers[:] + [6, 5, 3]
+
+1. numbers[0] = "ten"
+2. numbers[-1] = 1
+3. numbers[2:]
+4. 9 in numbers
+"""
