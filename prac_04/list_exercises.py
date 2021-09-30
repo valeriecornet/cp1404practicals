b23bb819f52c8788f2024d0c9f1ac8f4c83c6da0
valerie <valerie.cornet@my.jcu.edu.au>
2021-09-30 11:06:07 +1000

    Add basic list operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+"""Lists"""
+
+# Basic list operations
+
+numbers = []
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print("The first number is {} ".format(numbers[0]))
+print("The last number is {} ".format(numbers[-1]))
+print("The smallest number is {} ".format(min(numbers[:])))
+print("The largest number is {} ".format(max(numbers[:])))
+print("The average of the numbers is {} ".format(sum(numbers[:]) / 5))
+
+# Woefully inadequate security checker
+
+
