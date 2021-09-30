d4722c422e246042b2e1bb8a4fbdcb4db3ba0d69
valerie <valerie.cornet@my.jcu.edu.au>
2021-09-30 15:54:34 +1000

    finish exercises

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,4 +15,10 @@ print("The average of the numbers is {} ".format(sum(numbers[:]) / 5))
 
 # Woefully inadequate security checker
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
