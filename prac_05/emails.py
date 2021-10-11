c4c95d62e04eabda9a1a5ac5dba6e17187283216
valerie <valerie.cornet@my.jcu.edu.au>
2021-10-11 17:14:04 +1000

    Create email checker

---
+++ prac_05/emails.py
@@ -0,0 +1,24 @@
+"""Email storage"""
+
+def main():
+    emails_and_names = {}
+    email = input("Enter email: ")
+    while email != "":
+        name = extract_name(email)
+        name_confirmation = input("Is your name {} ? (Y/n) ".format(name))
+        if name_confirmation.upper() != "Y" and name_confirmation != "":
+            name = input("Enter your name:")
+        emails_and_names[email] = name
+        email = input("Enter email: ")
+
+    for email in emails_and_names:
+        print("{} ({})".format(emails_and_names[email], email))
+
+def extract_name(email):
+    fullname = email.split('@')[0]
+    separated_name = fullname.split(".")
+    print_name = " ".join(separated_name).title()
+    return print_name
+
+
+main()
